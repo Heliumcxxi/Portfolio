2b6621a2dbf3e41921a434b875bf9345d295a168
Heliumcxxi <Heliumcxxi@users.noreply.github.com>
2022-03-16 00:03:36 +0800

    amended

--- js/main.js
+++ js/main.js
@@ -21,4 +21,19 @@ $(document).ready(function () {
         type: 'image',
         gallery: { enabled: true }
     });
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    });
+
 }); 
