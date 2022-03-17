adf080419074e5c69baf23ba8f0bf37af21f848d
Heliumcxxi <Heliumcxxi@users.noreply.github.com>
2022-03-17 23:27:03 +0800

    Added responsive css file

--- js/main.js
+++ js/main.js
@@ -36,4 +36,24 @@ $(document).ready(function () {
         }
     });
 
-}); 
+    // Fixed nav bar
+
+    let nav_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed')
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+});
+
